337c30884bdd224cbea5b387d626a13815284356
Svetlana <sve1656@yandex.ru>
2024-04-23 19:48:02 +0300

    создан локальный репозиторий

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        CashbackHackService cashBack = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashBack.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
