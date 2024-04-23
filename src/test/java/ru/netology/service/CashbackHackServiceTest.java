c3bdca29724184de1a5401ee3c95f829e6cec6a4
Svetlana <sve1656@yandex.ru>
2024-04-23 20:06:37 +0300

    добавлена ветка JUNIT

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,41 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void shouldAdvicePayExtraUntilLimit() {
         CashbackHackService cashBack = new CashbackHackService();
-        int amount = 900;
+        int amount = 999;
 
         int actual = cashBack.remain(amount);
-        int expected = 100;
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAdvicePayExtraOverLimit() {
+        CashbackHackService cashBack = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashBack.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNoAdvicePayExtra() {
+        CashbackHackService cashBack = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashBack.remain(amount);
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
